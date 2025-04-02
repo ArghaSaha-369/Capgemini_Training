1f8763a6c3b34f468f6656421dcb5b828b9a88cd
UEM <arghasaha369@gmail.com>
2025-04-02 16:56:58 +0530

    Day 17 Updated

---
+++ day_17_exceptionHandling/ExceptionPropagation.java
@@ -0,0 +1,28 @@
+package day_17_exceptionHandling;
+
+public class ExceptionPropagation {
+	public static void main(String[] args) {
+		alpha();
+	}
+
+	public static void alpha() {
+		System.out.println("Alpha Method");
+		beta();
+	}
+
+	public static void beta() {
+		System.out.println("Beta Method");
+		gamma();
+	}
+
+	public static void gamma() {
+		System.out.println("Gamma Method");
+		try {
+		System.out.println(10/0);
+		}
+		catch(ArithmeticException e){
+			System.out.println("Arithmatic Exception Handled");
+		}
+		System.out.println("End of Gamma");
+	}
+}
---
+++ day_17_exceptionHandling/TryCatchBlock.java
@@ -0,0 +1,27 @@
+package day_17_exceptionHandling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TryCatchBlock {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		try {
+			System.out.println("Enter two numbers");
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+			System.out.println(x / y);
+			int[] arr = { 1, 2, 3, 4, 5 };
+			System.out.println("Enter the index to access");
+			System.out.println(arr[scan.nextInt()]);
+		} catch (ArithmeticException e) {
+			System.out.println("ArithmeticException handled");
+		} catch (InputMismatchException e) {
+			System.out.println("InputMismatchException handled");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("ArrayIndexOutOfBoundsException handled");
+		} catch (Exception e) {
+			System.out.println("Exception handled"); //You should always write your generic catch block at the end.
+		}
+	}
+}
